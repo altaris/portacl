48e7d309d35aaaa73914f861debc198abe9e8b8c
Cédric <nope-mcnopeface@asdf.org>
2019-08-04 21:20:55 +0200

    Linting

--- portacl.py
+++ portacl.py
@@ -1,15 +1,20 @@
+"""
+Portainer API reference:
+https://app.swaggerhub.com/apis/deviantony/Portainer/1.22.0/
+"""
+
 import docker
 import logging
 import os
 import requests
 
-DOCKER_CERT_PATH       = os.environ.get("DOCKER_CERT_PATH", "")
-DOCKER_HOST            = os.environ.get("DOCKER_HOST", "")
-DOCKER_TLS_VERIFY      = os.environ.get("DOCKER_TLS_VERIFY", "")
-LOGGING_LEVEL          = os.environ.get("LOGGING_LEVEL", "WARNING")
+DOCKER_CERT_PATH = os.environ.get("DOCKER_CERT_PATH", "")
+DOCKER_HOST = os.environ.get("DOCKER_HOST", "")
+DOCKER_TLS_VERIFY = os.environ.get("DOCKER_TLS_VERIFY", "")
+LOGGING_LEVEL = os.environ.get("LOGGING_LEVEL", "WARNING")
 PORTAINER_API_PASSWORD = os.environ.get("PORTAINER_API_PASSWORD", "")
 PORTAINER_API_USERNAME = os.environ.get("PORTAINER_API_USERNAME", "")
-PORTAINER_API_URL      = os.environ.get("PORTAINER_API_URL", "")
+PORTAINER_API_URL = os.environ.get("PORTAINER_API_URL", "")
 
 portainer_teams = {}
 portainer_token = None
@@ -41,7 +46,8 @@ def docker_listen():
     docker_client = docker.from_env()
     logging.info("Entering event loop")
     for event in docker_client.events(decode=True):
-        if event.get("Type") == "container" and event.get("Action") == "create":
+        if event.get("Type") == "container" and \
+           event.get("Action") == "create":
             ac = event.get("Actor")
             at = ac.get("Attributes")
             raw_teams = at.get("io.portainer.uac.teams")
@@ -65,36 +71,33 @@ def load_env():
         format='%(asctime)s [%(levelname)s] %(message)s',
         level={
             "CRITICAL": logging.CRITICAL,
-            "DEBUG":    logging.DEBUG,
-            "ERROR":    logging.ERROR,
-            "INFO":     logging.INFO,
-            "WARNING":  logging.WARNING
+            "DEBUG": logging.DEBUG,
+            "ERROR": logging.ERROR,
+            "INFO": logging.INFO,
+            "WARNING": logging.WARNING
         }[LOGGING_LEVEL])
     logging.debug("DOCKER_CERT_PATH = \"{}\""
-        .format(DOCKER_CERT_PATH))
+                  .format(DOCKER_CERT_PATH))
     logging.debug("DOCKER_HOST = \"{}\""
-        .format(DOCKER_HOST))
+                  .format(DOCKER_HOST))
     logging.debug("DOCKER_TLS_VERIFY = \"{}\""
-        .format(DOCKER_TLS_VERIFY))
+                  .format(DOCKER_TLS_VERIFY))
     logging.debug("LOGGING_LEVEL = \"{}\""
-        .format(LOGGING_LEVEL))
+                  .format(LOGGING_LEVEL))
     logging.debug("PORTAINER_API_PASSWORD = \"{}\""
-        .format(PORTAINER_API_PASSWORD))
+                  .format(PORTAINER_API_PASSWORD))
     logging.debug("PORTAINER_API_USERNAME = \"{}\""
-        .format(PORTAINER_API_USERNAME))
+                  .format(PORTAINER_API_USERNAME))
     logging.debug("PORTAINER_API_URL = \"{}\""
-        .format(PORTAINER_API_URL))
+                  .format(PORTAINER_API_URL))
 
 
 def portainer_init():
-    """
-    Reference: https://app.swaggerhub.com/apis/deviantony/Portainer/1.22.0/#/auth/AuthenticateUser
-    """
-    logging.info("Authenticating to portainer api {}".format(PORTAINER_API_URL))
-    r = requests.post(PORTAINER_API_URL + "/auth",
-        json={
-            "Password": PORTAINER_API_PASSWORD,
-            "Username": PORTAINER_API_USERNAME
+    logging.info("Authenticating to portainer api {}"
+                 .format(PORTAINER_API_URL))
+    r = requests.post(PORTAINER_API_URL + "/auth", json={
+        "Password": PORTAINER_API_PASSWORD,
+        "Username": PORTAINER_API_USERNAME
     })
     r.raise_for_status()
     global portainer_token
@@ -117,10 +120,9 @@ def portainer_request(method, url, json={}):
         "POST": requests.post,
         "PUT": requests.put
     }[method]
-    r = f(PORTAINER_API_URL + url, json=json,
-        headers={
-            "Authorization": "Bearer " + portainer_token
-        })
+    r = f(PORTAINER_API_URL + url, json=json, headers={
+        "Authorization": "Bearer " + portainer_token
+    })
     r.raise_for_status()
     return r.json()
 
