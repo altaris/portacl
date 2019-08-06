d1ceac1c4cb8e5558392e44719bd0fec21bbe113
Cédric <nope-mcnopeface@asdf.org>
2019-08-05 20:30:34 -0400

    ACL update support

--- portacl.py
+++ portacl.py
@@ -183,6 +183,7 @@ def portainer_request(method, url, json={}):
             raise e
 
 
+@static_var("uac", {})
 def portainer_set_uac(**kwargs):
     """Updates the uac of a docker ressource.
 
@@ -201,28 +202,68 @@ def portainer_set_uac(**kwargs):
     elif ressource_type is None:
         raise ValueError("UAC must specify a docker ressource type.")
     public = (kwargs.get("public").lower() == "true")
+    subressource_ids = kwargs.get("subressource_ids", [])
     teams = [] if kwargs.get("teams") in [None, ""] else \
         [team_id(x.strip()) for x in str(kwargs.get("teams")).split(',')]
     users = [] if kwargs.get("users") in [None, ""] else \
         [user_id(x.strip()) for x in str(kwargs.get("users")).split(',')]
     if not public and len(teams) == 0 and len(users) == 0:
-        logging.warning(f'Empty UAC for ressource {ressource_id}, skipping')
+        logging.warning(f'ACL for ressource {ressource_id} is empty, skipping')
     else:
-        try:
-            portainer_request("DELETE", "/resource_controls/" + ressource_id)
-        except requests.HTTPError:
-            pass
-        try:
-            portainer_request("POST", "/resource_controls", {
-                "Public": public,
-                "ResourceID": ressource_id,
-                "SubResourceIDs": kwargs.get("subressource_ids", []),
-                "Teams": teams,
-                "Type": ressource_type,
-                "Users": users
-            })
-        except requests.HTTPError as e:
-            logging.error("Portainer API request failed: " + str(e))
+        if ressource_id in portainer_set_uac.uac:
+            current_public = portainer_set_uac.uac["io.portainer.uac.public"]
+            current_subressource_ids = \
+                portainer_set_uac.uac["io.portainer.subressources"]
+            current_teams = portainer_set_uac.uac["io.portainer.uac.teams"]
+            current_users = portainer_set_uac.uac["io.portainer.uac.users"]
+            new_public = current_public or public
+            new_subressource_ids = list(set(
+                current_subressource_ids + subressource_ids))
+            new_teams = list(set(current_teams + teams))
+            new_users = list(set(current_users + users))
+            try:
+                portainer_request(
+                    "PUT",
+                    "/resource_controls/" + ressource_id,
+                    {
+                        "Public": new_public,
+                        "SubResourceIDs": new_subressource_ids,
+                        "Teams": new_teams,
+                        "Users": new_users
+                    })
+                portainer_set_uac.uac[ressource_id] = {
+                    "io.portainer.uac.public": new_public,
+                    "io.portainer.subressources": new_subressource_ids,
+                    "io.portainer.uac.teams": new_teams,
+                    "io.portainer.uac.users": new_users
+                }
+            except requests.HTTPError:
+                logging.error(
+                    f'Could not update ACL of {ressource_id}: {str(e)}')
+        else:
+            try:
+                portainer_request(
+                    "DELETE", "/resource_controls/" + ressource_id)
+            except requests.HTTPError:
+                pass
+            try:
+                portainer_request("POST", "/resource_controls", {
+                    "Public": public,
+                    "ResourceID": ressource_id,
+                    "SubResourceIDs": subressource_ids,
+                    "Teams": teams,
+                    "Type": ressource_type,
+                    "Users": users
+                })
+                portainer_set_uac.uac[ressource_id] = {
+                    "io.portainer.uac.public": public,
+                    "io.portainer.subressources": subressource_ids,
+                    "io.portainer.uac.teams": teams,
+                    "io.portainer.uac.users": users
+                }
+            except requests.HTTPError as e:
+                logging.error(
+                    f'Could not create new ACL for {ressource_id}: {str(e)}')
 
 
 @static_var("teams", {})
